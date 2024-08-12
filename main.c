2ff31ec40b8aaa02d24d6d8c143b899e472da036
guilhermeP4es <145516623+Gu1lhermeP4es@users.noreply.github.com>
2024-08-12 20:17:44 -0300

    Update main.c
    Funções inseridas: criarNo, enfileirar, inicializar fila; 
    Especificação do Nó cabeça e dos nós da fila

--- main.c
+++ main.c
@@ -3,6 +3,49 @@
 #include <string.h>
 #include "pessoa.h"
 
+typedef struct Pessoa{ // especificação do nó da fila
+char nome[30];
+char cpf[12];
+int idade;
+int deficiente;
+struct Pessoa* prox;} No;
+
+typedef struct gerenciaFila{ // Nó cabeça
+  No* inicio;
+  No* fim;}fila;
+
+//Cria nó do tipo de Cliente
+No* criarNo(char nome[], char cpf[],int idade, int deficiente){
+  No* novoCliente=(No*)malloc(sizeof(No));
+  strcpy(novoCliente->nome,nome);
+  strcpy(novoCliente->cpf,cpf);
+  novoCliente->idade=idade;
+  novoCliente->deficiente=deficiente;
+  novoCliente->prox=NULL;
+  return novoCliente;
+}
+
+void inicializaFila(fila* fila){
+  fila->inicio=NULL;
+  fila->fim=NULL;
+}
+
+void enfileirar(fila* fila, char nome[], char cpf[], int idade, int deficiente){
+  No* novoCliente=criarNo(nome, cpf, idade, deficiente);
+  if(novoCliente==NULL){
+    printf("Falha na alocação\n");
+    return;
+  }
+  if(fila->inicio==NULL){ //caso fila esteja vazia
+    fila->inicio=novoCliente;
+    fila->fim=novoCliente;
+  }
+  else{
+    fila->fim->prox=novoCliente;// caso fila já tenha alguém
+    fila->fim=novoCliente;
+  }
+}
+
 int main() {
     srand(rand()); // Gera uma seed aleatoria pro RNG
 
